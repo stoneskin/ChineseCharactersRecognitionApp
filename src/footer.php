677198e2f16da96445b8dd7395ea94b35f82f8d3
lightspd547 <lsun0547@gmail.com>
2023-10-14 20:42:58 -0400

    changed html to php and added header.php and footer.php

---
+++ src/footer.php
@@ -0,0 +1,9 @@
+        <div class="footer">
+            <div class="c-mlccc-2023">
+                <div class="mlccc-2023">© MLCCC 2023</div>
+            </div>
+        </div>
+    </div>
+    </body>
+
+</html>
--- src/login.php
+++ src/login.php
@@ -1,27 +1,4 @@
-<html>
-<head>
-    <title> MLCCC 识字比赛
-    </title>
- 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-   <link rel="stylesheet" type="text/css" href="css\styles.css" />
-</head>
-
-<body>
-
-    <div class="responsive">
-        <div class="header">
-            <div class="header2">
-                <div class="logo">
-                    <img class="logo2" src="images/logo.png" />
-                </div>
-                <div class="mlccc-words-test">MLCCC Words Test</div>
-            </div>
-        </div>
+<?php include "header.php" ?>
         <div class="two-column-frame container">
             <div class="row">
          
@@ -46,7 +23,7 @@
                 </div>
                 <div class="frame-botton">
                     <div class="frame-botton2">
-                        <div class="button" onclick="(()=>{window.location.assign('studentInfo.html')})()">
+                        <div class="button" onclick="(()=>{window.location.assign('studentInfo.php')})()">
                             <div class="submit" >Log In</div>
                         </div>
                     </div>
@@ -54,7 +31,7 @@
                 <div class="don-t-have-an-account-sign-up">
                     <div class="don-t-have-an-account-sign-up2">
                         <span><span class="don-t-have-an-account-sign-up-2-span">Don’t have an account? </span><span
-                                class="don-t-have-an-account-sign-up-2-span2"><a href="signup.html">Sign Up</a></span></span>
+                                class="don-t-have-an-account-sign-up-2-span2"><a href="signup.php">Sign Up</a></span></span>
                     </div>
                 </div>
             </div>
@@ -80,22 +57,13 @@
                 </div>
                 <div class="frame-botton">
                     <div class="frame-botton2">
-                        <div class="button button-wide" onclick="(()=>{window.location.assign('studentInfo.html')})()">
+                        <div class="button button-wide" onclick="(()=>{window.location.assign('studentInfo.php')})()">
                             <div class="submit">Guest Log In</div>
                         </div>
                     </div>
                 </div>
             </div>
             
-            </div>
         </div>
    
-        <div class="footer">
-            <div class="c-mlccc-2023">
-                <div class="mlccc-2023">© MLCCC 2023</div>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
+<?php include "footer.php" ?>
