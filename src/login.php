2968928f2fe7e21a594f376a333b88e7d5c4f232
magicArc <ethanguo01@gmail.com>
2023-10-14 23:24:23 -0400

    Completed login page

--- src/login.php
+++ src/login.php
@@ -1,3 +1,27 @@
+<?php
+include "connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+    // username and password sent from form 
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        $myemail = $_POST["email"];
+        $mypassword = $_POST["password"]; 
+
+        $sql = "SELECT id FROM user WHERE Email = '$myemail' and Password = '$mypassword'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows === 1) {
+            header("Location: studentInfo.html");
+            exit();        
+        } else {
+            $error = "Invalid email or password";
+            header("Location: login.php?error=" . urlencode($error));
+            exit();
+        }
+    }
+}
+?>
+
 <html>
 <head>
     <title> MLCCC 识字比赛
@@ -26,39 +50,44 @@
             <div class="row">
          
             <div class="frame col-md-5 col-sm-8">
-                <div class="form-title">Login</div>
-                <div class="input-component">
-                    <div class="label-frame">
-                        <div class="label">Email</div>
-                    </div>
-                    <div >
-                        <input type="email" class="textbox-frame form-control" id="txtEmail" aria-describedby="emailHelp" placeholder="Enter email">
-                        
-                    </div>
-                </div>
-                <div class="input-component">
-                    <div class="label-frame">
-                        <div class="label">Password</div>
-                    </div>
-                    <div >
-                        <input type="password" class="textbox-frame form-control" id="txtPassword" placeholder="Password">
-                    </div>
-                </div>
-                <div class="frame-botton">
-                    <div class="frame-botton2">
-                        <div class="button" onclick="(()=>{window.location.assign('studentInfo.html')})()">
-                            <div class="submit" >Log In</div>
+                <form action="login.php" method="post">
+                    <div class="form-title">Login</div>
+                    <div class="input-component">
+                        <div class="label-frame">
+                            <div class="label">Email</div>
+                        </div>
+                        <div >
+                            <input type="email" name="email" class="textbox-frame form-control" id="txtEmail" aria-describedby="emailHelp" placeholder="Enter email" />
                         </div>
                     </div>
-                </div>
-                <div class="don-t-have-an-account-sign-up">
-                    <div class="don-t-have-an-account-sign-up2">
-                        <span><span class="don-t-have-an-account-sign-up-2-span">Don’t have an account? </span><span
-                                class="don-t-have-an-account-sign-up-2-span2"><a href="signup.html">Sign Up</a></span></span>
+                    <div class="input-component">
+                        <div class="label-frame">
+                            <div class="label">Password</div>
+                        </div>
+                        <div >
+                            <input type="password" name="password" class="textbox-frame form-control" id="txtPassword" placeholder="Password" />
+                        </div>
                     </div>
-                </div>
+                    <?php
+                        if (isset($_GET['error'])) {
+                            $error = $_GET['error'];
+                            echo "<p style='color: red;'>$error</p>";
+                        }
+                    ?>
+                    <div class="frame-botton">
+                        <div class="frame-botton2">
+                            <button class="button submit" type="submit">Log In</button>
+                        </div>
+                    </div>
+                    <div class="don-t-have-an-account-sign-up">
+                        <div class="don-t-have-an-account-sign-up2">
+                            <span><span class="don-t-have-an-account-sign-up-2-span">Don’t have an account? </span><span
+                                    class="don-t-have-an-account-sign-up-2-span2"><a href="signup.html">Sign Up</a></span></span>
+                        </div>
+                    </div>
+                </form>
             </div>
-           
+            
             <div class="frame col-md-5 col-sm-8">
                 <div class="form-title">Guest Judge Login</div>
                 <div class="input-component">
@@ -66,7 +95,7 @@
                         <div class="label">UserName</div>
                     </div>
                     <div>
-                        <input  class="textbox-frame form-control" id="txtUserName"  placeholder="">
+                        <input  class="textbox-frame form-control" id="txtUserName"  placeholder="" />
                 
                     </div>
                 </div>
@@ -75,7 +104,7 @@
                         <div class="label">AccessKey</div>
                     </div>
                     <div >
-                        <input type="password" class="textbox-frame form-control" id="txtAccessKey" placeholder="access key">
+                        <input type="password" class="textbox-frame form-control" id="txtAccessKey" placeholder="access key" />
                     </div>
                 </div>
                 <div class="frame-botton">
