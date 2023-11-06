7dd41de98f572459e7f8af22a6366d874a3986df
lsun0547 <lsun0547@gmail.com>
2023-11-05 19:26:18 -0500

    done wordlibrary sql script

---
+++ dbscript/4_insert_words.sql
@@ -0,0 +1,13 @@
+INSERT INTO `wordslibrary` (`ID`, `Words`, `Level`) VALUES
+    (NULL, '你好', '1'),
+    (NULL, '再见', '1'),
+    (NULL, '大', '1'),
+    (NULL, '小', '1'),
+    (NULL, '你好', '2'),
+    (NULL, '再见', '2'),
+    (NULL, '大', '2'),
+    (NULL, '小', '2'),
+    (NULL, '你好', '3'),
+    (NULL, '再见', '3'),
+    (NULL, '大', '3'),
+    (NULL, '小', '3');
