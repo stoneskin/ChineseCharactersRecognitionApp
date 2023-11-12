f8813bcdb3c7651b0b8c3aec44dfc5113d6d8e33
lsun0547 <lsun0547@gmail.com>
2023-11-12 11:22:22 -0500

    finished activities and records tables

---
+++ dbscript/5_activities_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `ccrapp`.`activities` (
+    `ActivityID` INT NOT NULL AUTO_INCREMENT,
+    `EventID` INT NOT NULL,
+    `StudentName` TEXT NOT NULL,
+    `StudentID` INT NULL,
+    `JudgeName` TEXT NOT NULL,
+    `Level` INT NOT NULL,
+    `StartTime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `CompletedTime` DATETIME on update CURRENT_TIMESTAMP NULL,
+    PRIMARY KEY (`ActivityID`)
+    ) ENGINE = InnoDB; 
