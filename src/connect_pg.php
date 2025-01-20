fc33c0f81f3199caf79d246b33f332b57831cc99
wsun <git>
2025-01-20 16:44:30 -0500

    Revert "update eventinfo with filter"
    This reverts commit 4b1bc7aa91c7045b5cc009147355bdd96efcb72c.

--- src/connect_pg.php
+++ src/connect_pg.php
@@ -1,9 +1,9 @@
 <?php
-$host = "ccqnant9i80rgh.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$host = "your_host";
 $port = "5432";
-$dbname = "d5o0uel6beunq7";
-$user = "u7cifl6aggh7mr";
-$password = "pc93d52331178c261f14c7c3bd07c19c8d5b1016a50c599d939ce08cf9bef95f8";
+$dbname = "your_dbname";
+$user = "your_user";
+$password = "your_password";
 
 $conn_pg = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
--- src/eventInfo.php
+++ src/eventInfo.php
@@ -18,45 +18,17 @@ if ($row != null && $row['EventName'] != null) {
     $tableTitle = $row['EventName']. " Activity List";
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-// Check if form is submitted
-    $isTestChecked = isset($_POST['testActivities']);
-    $isPracticeChecked = isset($_POST['practiceActivities']);
-    if (!$isTestChecked && !$isPracticeChecked) {
-        echo "<script>alert('Please check at least one activity type.');</script>";
-    } else {
-        $activitySql = "SELECT StudentName, StudentID, ActivityID, g.GradeName, FinalScore, TimeSpent, isPractice FROM activities a join grade g on g.GradeId=a.level WHERE EventID = ?";
-
-        if ($isTestChecked && !$isPracticeChecked) {
-            $activitySql .= " AND isPractice = 0";
-        } elseif (!$isTestChecked && $isPracticeChecked) {
-            $activitySql .= " AND isPractice = 1";
-        }
-
-        $activitySql .= " ORDER BY ActivityID DESC";
-
-        if ($stmtActivity = $conn->prepare($activitySql)) {
-            $stmtActivity->bind_param("i", $eventID);
-            $stmtActivity->execute();
-        } else {
-            die("Errormessage: " . $conn->error);
-        }
-        $resultActivity = $stmtActivity->get_result();
-    }
-} else {
-    // Default query to show test activities
-    $activitySql = "SELECT StudentName, StudentID, ActivityID, g.GradeName, FinalScore, TimeSpent, isPractice FROM activities a join grade g on g.GradeId=a.level WHERE EventID = ? AND isPractice = 0 ORDER BY ActivityID DESC";
-    if ($stmtActivity = $conn->prepare($activitySql)) {
-        $stmtActivity->bind_param("i", $eventID);
-        $stmtActivity->execute();
-    } else {
-        die("Errormessage: " . $conn->error);
-    }
-    $resultActivity = $stmtActivity->get_result();
-
-    $isTestChecked = true;
-    $isPracticeChecked = false;
+//get activity list by EventID
+$activitySql = "SELECT StudentName, StudentID, ActivityID, g.GradeName, FinalScore, TimeSpent,isPractice FROM activities a join grade g on g.GradeId=a.level WHERE EventID = ? ORDER BY ActivityID DESC";
+if($stmtActivity = $conn->prepare($activitySql)){
+    $stmtActivity->bind_param("i", $eventID);
+    $stmtActivity->execute();
+}else{
+    die("Errormessage: ". $conn->error);
 }
+$resultActivity = $stmtActivity->get_result();
+
+
 ?>
 
 <div class="container">
@@ -64,15 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="frame-main col-md-12 col-sm-12">
 
         <h2 class="border-bottom border-dark"><?php echo $tableTitle?></h2>
-
             <div class="col-md-12 col-sm-12">
-                <div style="font-size: small">
-                <form method="post" action="">
-                <input type="checkbox" onchange="this.form.submit()" name="testActivities" id="testActivities" value="1" <?php echo $isTestChecked ? 'checked' : ''; ?>>   <label for="testActivities">Test activities</label>
-                <input type="checkbox" onchange="this.form.submit()"  name="practiceActivities" id="practiceActivities" value="1" <?php echo $isPracticeChecked ? 'checked' : ''; ?>>  <label for="practiceActivities">Practice activities</label>
-                <input type="submit" value="Filter" style="display:none">
-                </form>
-                </div>
                 <table>
                     <tr>
                         <th class="text-center p-1">Student Name #</th>
